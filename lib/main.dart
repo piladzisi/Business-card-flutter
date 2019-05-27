43f6da02212fb996616f18b04ddf885189fa24aa
piladzisi <piladzisi@gmail.com>
2019-05-27 17:46:30 +0300

    background image(decorationbox) and other boxes

--- lib/main.dart
+++ lib/main.dart
@@ -9,33 +9,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Column(
+        backgroundColor: Colors.blueGrey.shade400,
+        body: Stack(
+          fit: StackFit.expand,
           children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/robot.jpg'),
-            ),
-            Text(
-              'Anna Steel',
-              style: TextStyle(
-                letterSpacing: 5.0,
-                fontFamily: 'Blanka',
-                fontSize: 40.0,
-                color: Colors.white,
+            Opacity(
+              opacity: 0.89,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/moon.jpg'), fit: BoxFit.cover),
+                ),
               ),
             ),
-            Text(
-              'Flutter developer',
-              style: TextStyle(
-                fontFamily: 'Acme',
-                fontSize: 20.0,
-                color: Colors.white,
-              ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundImage: AssetImage('images/robot.jpg'),
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  'Anna Steel',
+                  style: TextStyle(
+                    letterSpacing: 5.0,
+                    fontFamily: 'Blanka',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  'Flutter developer'.toUpperCase(),
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.blueGrey.shade200,
+                    letterSpacing: 0.1,
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                Container(
+                  color: Colors.grey.shade700,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phonelink_ring,
+                        color: Colors.blueGrey.shade200,
+                        size: 30.0,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        '+371 263 80 104',
+                        style: TextStyle(
+                          color: Colors.blueGrey.shade200,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
-        )),
+        ),
       ),
     );
   }
